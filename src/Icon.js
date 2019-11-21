d2da4970642d02d79ffbc2c0d3eeac1ea4be0595
Margarida <margaridajustoribeiro@gmail.com>
2019-11-21 19:33:33 +0000

    Updated the export to the updated sitax without the class/extends/render

--- src/Icon.js
+++ src/Icon.js
@@ -1,20 +1,18 @@
 import React from "react";
 
-export default class Icon extends React.Component {
-  render = () => {
-    return (
-      <div className="weather-icon">
-        <img
-          src="http://openweathermap.org/img/wn/10d@2x.png"
-          alt="light rain"
-          id="icon"
-          className="mx-auto"
-        />
+export default function Icon() {
+  return (
+    <div className="weather-icon">
+      <img
+        src="http://openweathermap.org/img/wn/10d@2x.png"
+        alt="light rain"
+        id="icon"
+        className="mx-auto"
+      />
 
-        <div id="description" className="weather-description">
-          few clouds
-        </div>
+      <div id="description" className="weather-description">
+        few clouds
       </div>
-    );
-  };
+    </div>
+  );
 }
