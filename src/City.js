426a2c77a8a7c39e04e6d2c0f05fcf6121ebd913
Margarida <margaridajustoribeiro@gmail.com>
2019-11-21 20:13:43 +0000

    Updated the export function City to the updated sitax without the class/extends/render & deleted the id

--- src/City.js
+++ src/City.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-export default class City extends React.Component {
-  render = () => {
-    return (
-      <div className="City">
-        <span id="city-name">Lisboa</span>
-      </div>
-    );
-  };
+export default function City() {
+  return (
+    <div className="City">
+      <span>Lisboa</span>
+    </div>
+  );
 }
