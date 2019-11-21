501234f2d965f80870353f2171e5918ce598ee42
Margarida <margaridajustoribeiro@gmail.com>
2019-11-21 19:52:22 +0000

    Started the test for show Date/Time Details

--- src/Details.js
+++ src/Details.js
@@ -28,3 +28,50 @@ export default function Details() {
     </div>
   );
 }
+
+/*
+function updateDateTime() {
+  let [hourDisplay, setHourDisplay] = useState() //hourDisplay = document.querySelector("#current-time")//;
+  let [currentTime, setCurrentTime] = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekdayDisplay = document.querySelector("#week-day");
+  let currentDay = new Date();
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let weekday = weekdays[currentDay.getDay()];
+  let dateDisplay = document.querySelector("#current-date");
+  let date = new Date();
+  let months = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12"
+  ];
+  let month = months[date.getMonth()];
+  let day = date.getDate();
+  let year = date.getFullYear();
+
+  hourDisplay.innerHTML = `${hours}:${minutes}`;
+  weekdayDisplay.innerHTML = `${weekday}`;
+  dateDisplay.innerHTML = `${year}.${month}.${day}`;
+}
+*/
