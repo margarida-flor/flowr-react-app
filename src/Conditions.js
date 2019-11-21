7cf64c2a928c1dac331102f881cffe8c325e04db
Margarida <margaridajustoribeiro@gmail.com>
2019-11-21 20:14:19 +0000

    Updated the export function Conditions to the updated sitax without the class/extends/render & deleted the id's

--- src/Conditions.js
+++ src/Conditions.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-export default class Conditions extends React.Component {
-  render = () => {
-    return (
-      <div className="current-conditions">
-        Wind:
-        <span id="wind"> 10 </span>km/h
-        <div>
-          Humidity:
-          <span id="humidity"> 50</span>%
-        </div>
+export default function Conditions() {
+  return (
+    <div className="current-conditions">
+      Wind:
+      <span> 10 </span>km/h
+      <div>
+        Humidity:
+        <span> 50</span>%
       </div>
-    );
-  };
+    </div>
+  );
 }
