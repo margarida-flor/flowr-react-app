a93b414ed93cb714ee1784fbcabe0320c8025111
Margarida <margaridajustoribeiro@gmail.com>
2019-11-28 14:14:03 +0000

    Fixed the hours

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -31,8 +31,8 @@ export default function FormattedDate(props) {
   let month = months[props.date.getMonth()];
   let year = props.date.getFullYear();
   let hours = props.date.getHours();
-  if (minutes < 10) {
-    minutes = `0${hours}`;
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
